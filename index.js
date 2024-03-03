b6756b167d99ec2551aa64d8dac97ce2143e0a6c
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-03-03 03:38:36 -0300

    site

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require("dotenv/config");
 
 const bot = require("./client/index");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
 
 bot.login(process.env.TOKEN);
