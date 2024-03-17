3303cc0b58cb6467d8576a09fd955ba8e972d2bb
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-03-16 21:38:16 -0300

    Command Handler

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 require("dotenv/config");
 
-const { Client, IntentsBitField, Collection, REST, Routes } = require('discord.js');
-const { glob } = require("glob");
+const { Client, IntentsBitField } = require('discord.js');
+const { CommandHandler } = require("djs-commander");
 const bot = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
     ],
-});
-
-bot.commands = new Collection();
-
-bot.once("ready", async () => {
-    const events = await glob("src/events/**/*.js");
-    const commands = await glob("src/commands/**/*.js");
-
-    for (const evt of events) {
-        const event = require(`${__dirname}\\${evt}`)
-
-        bot.on(event.name, async (...args) => await event.run(...args));
-    }
-
-    for(const cmd of commands){
-        const command = require(`${__dirname}\\${cmd}`);
-
-        bot.commands.set(command.name ?? command.data.name, command);
-    }
 });
 
 bot.login(process.env.TOKEN);
