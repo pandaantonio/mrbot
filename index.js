1065a29966cce3b057e58ff0eee307938ef2ff42
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-03-17 12:55:14 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv/config");
 
 const { Client, IntentsBitField } = require('discord.js');
+const { promisify } = require("util")
 const { glob } = require("glob");
+const pg = promisify(glob);
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -10,7 +12,12 @@ const client = new Client({
 });
 
 client.login(process.env.TOKEN).then(async () => {
-    const eventFiles = await glob("src/events/**/*.js");
-
+    const eventFiles = await pg("src/events/**/*.js");
     console.log(eventFiles);
+
+    for(const file of eventFiles){
+        const event = require(`${__dirname}\\${file}`);
+
+        client.on(event.name, async (...args) => await event.run(...args));
+    }
 });
